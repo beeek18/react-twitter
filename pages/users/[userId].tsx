7b84a97f07757d249d0693d00413710048c469c5
Bekzodbek <kodirov2000@list.ru>
2023-04-04 22:25:51 +0300

    Users and Individual User Profile

---
+++ pages/users/[userId].tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+import UserHero from "@/components/users/UserHero";
+import Header from "@/components/Header";
+import useUser from "@/hooks/useUser";
+import UserBio from "@/components/users/UserBio";
+
+const UserView = () => {
+  const route = useRouter();
+  const { userId } = route.query;
+
+  const { data: fetchedUser, isLoading } = useUser(userId as string);
+
+  if (isLoading || !fetchedUser) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Header showBackArrow label={fetchedUser?.name} />
+      <UserHero userId={userId as string} />
+      <UserBio userId={userId as string} />
+    </>
+  );
+};
+
+export default UserView;
